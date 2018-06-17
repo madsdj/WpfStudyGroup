a30ebb19cfd4dc60999453fa609f7f52332a26f5
Mads DJ <madsdj@gmail.com>
2018-06-17 22:14:43 +0200

    Added MVVM project

---
+++ Mvvm/ObservableObject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
+
+namespace Mvvm
+{
+    public abstract class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void RaisePropertyChanged<T>(Expression<Func<T>> propertyExpression)
+        {
+            if (propertyExpression == null) throw new ArgumentNullException(nameof(propertyExpression));
+
+            var memberExpression = propertyExpression.Body as MemberExpression;
+            if (memberExpression == null) throw new ArgumentException("The expression is not a member access expression.", nameof(propertyExpression));
+
+            RaisePropertyChanged(memberExpression.Member.Name);
+        }
+    }
+}
