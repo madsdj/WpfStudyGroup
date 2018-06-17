d47905687d292616f38ad1698709747f4e98d968
Mads DJ <madsdj@gmail.com>
2018-06-18 01:40:50 +0200

    Implemented some more view stuff

---
+++ AssetManager.Views/Converters/ObjectToPropertiesConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Data;
+
+namespace AssetManager.Views.Converters
+{
+    public class ObjectToPropertiesConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null) return null;
+
+            return value
+                .GetType()
+                .GetProperties()
+                .OrderBy(pi => pi.Name)
+                .Select(pi => new { pi.Name, Value = pi.GetValue(value) })
+                .ToImmutableList();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
