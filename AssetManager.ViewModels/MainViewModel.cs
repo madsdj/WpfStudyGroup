719b5ddb0360fdf868b07bf290018056723d26fa
Mads DJ <madsdj@gmail.com>
2018-06-17 22:46:17 +0200

    Added ViewModels and Views projects

---
+++ AssetManager.ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AssetManager.Domain.Repositories;
+using Mvvm;
+
+namespace AssetManager.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        private readonly IAssetRepository _assetRepository;
+
+        public MainViewModel(IAssetRepository assetRepository)
+        {
+            _assetRepository = assetRepository ?? throw new ArgumentNullException(nameof(assetRepository));
+        }
+    }
+}
