46fe4c200c1161e6bede405a3e2b2e4af32f91da
Mads DJ <madsdj@gmail.com>
2018-09-07 18:54:50 +0200

    Added fallback value to AssetToImageConverter

--- AssetManager.Views/Converters/AssetToImageConverter.cs
+++ AssetManager.Views/Converters/AssetToImageConverter.cs
@@ -2,17 +2,19 @@
 using System.Collections.Generic;
 using System.Globalization;
 using System.Windows.Data;
-using System.Windows.Media;
 
 namespace AssetManager.Views.Converters
 {
     public class AssetToImageConverter : IValueConverter
     {
+        public string FallbackValue { get; set; }
         public Dictionary<Type, string> ImageSources { get; } = new Dictionary<Type, string>();
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return ImageSources[value.GetType()];
+            if (value == null) return null;
+            if (ImageSources.TryGetValue(value.GetType(), out string s)) return s;
+            return FallbackValue;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
