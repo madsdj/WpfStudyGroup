f601cae88f413bd6acd80036cd81576cb49786fb
Mads DJ <madsdj@gmail.com>
2018-09-07 18:09:02 +0200

    Added Start and Stop buttons + commands

--- AssetManager.ViewModels/AssetDetailsViewModel.cs
+++ AssetManager.ViewModels/AssetDetailsViewModel.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using AssetManager.Domain.Models;
 using Mvvm;
 
@@ -18,5 +19,19 @@ namespace AssetManager.ViewModels
         }
 
         public Asset Model { get; }
+
+        public ICommand StartCommand => new RelayCommand(Start);
+
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICommand StopCommand => new RelayCommand(Stop);
+
+        public void Stop()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
